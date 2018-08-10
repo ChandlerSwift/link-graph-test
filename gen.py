e0c8baab5ad469556d732601c1f2b73009b6801c
Chandler Swift <chandler@chandlerswift.com>
2018-08-10 16:24:05 -0500

    Add files in directory

--- gen.py
+++ gen.py
@@ -2,8 +2,11 @@
 
 import string # gen page names
 import random # gen page names
+import os # create directories
 
 NUM_PAGES=int(input('how many pages to generate: '))
+directory='./html'
+
 
 def rand_str(length):
     return ''.join([random.choice(string.ascii_letters) for n in range(length)])
@@ -11,19 +14,26 @@ def rand_str(length):
 # generate pages
 pages = [rand_str(12) for n in range(NUM_PAGES)]
 
+if not os.path.exists(directory):
+    os.makedirs(directory)
+else:
+    print('Warning: html directory already exists.')
+
 for page in pages:
+    print('Generating %s/%s.html' % (directory, page))
     linked_pages = random.sample(pages, random.randrange(NUM_PAGES))
     page_content = "<!DOCTYPE html><html><head><title>" + page + "</title></head><body>"
     for linked_page in linked_pages:
         page_content += "<a href=\"%s.html\">%s</a><br>" % (linked_page, linked_page)
     page_content += "</body></html>"
-    with open(page+'.html', 'w') as f:
+    with open(directory+'/'+page+'.html', 'w') as f:
         f.write(page_content)
 
 # generate index page
+print('Generating %s/index.html' % directory)
 page_content = "<!DOCTYPE html><html><head><title>" + page + "</title></head><body>"
 for page in pages:
     page_content += "<a href=\"%s.html\">%s</a><br>" % (page, page)
 page_content += "</body></html>"
-with open('index.html', 'w') as f:
+with open(directory+'/index.html', 'w') as f:
     f.write(page_content)
